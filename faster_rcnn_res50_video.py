9db596db6af04193f5ab6991580944a24510c3ac
JinFree <ytrqwe12@gmail.com>
2020-09-25 14:23:04 +0900

    videos

--- faster_rcnn_res50_video.py
+++ faster_rcnn_res50_video.py
@@ -80,5 +80,16 @@ def main(VideoPath):
     Video(VideoPath)    
     
 if __name__=='__main__':
-    videoPath = 'challenge.mp4'
+    ## TX2_ONBOARD_CAMERA
+    #videoPath = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+    
+    ##LOGITECH_C920_CAMERA
+    #videoPath = "v4l2src device=/dev/video1 ! video/x-raw, width=640, height=480, format=(string)YUY2,framerate=30/1 ! videoconvert ! video/x-raw,width=640,height=480,format=BGR ! appsink"
+    
+    videoPath = "challenge.mp4"
     main(videoPath)
+    
+    
+    
+    
+    
